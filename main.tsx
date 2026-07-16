427d7c50b15e03d1b6796d7d7022cc8b14f83f18
Pen <kk4e18@gmail.com>
2026-07-16 18:36:14 +0800

    feat(analytics): 統一五站導覽事件契約

--- main.tsx
+++ main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import KiwimuUniverseRail from './components/KiwimuUniverseRail';
+import { trackEvent } from './utils/crossSiteTracking';
+import { getAuthSupabaseClient } from './utils/supabaseAuthBridge';
 import './styles/tailwind.css';
 import './styles/kiwimu-universe.css';
 
@@ -15,7 +17,13 @@ const isOgRenderRoute = window.location.pathname.includes('/og-render');
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    {!isOgRenderRoute && <KiwimuUniverseRail currentSite="kiwimu" />}
+    {!isOgRenderRoute && (
+      <KiwimuUniverseRail
+        currentSite="kiwimu"
+        authClient={getAuthSupabaseClient()}
+        onTrack={trackEvent}
+      />
+    )}
     <App />
   </React.StrictMode>
 );
